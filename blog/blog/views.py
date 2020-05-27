5fdc4c9d36b91b82049ebbcba926976d4b34d182
danieldsouza <ddsouza@umich.edu>
2020-05-26 23:34:26 -0400

    # Chapters till 8

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
+
 # Create your views here.
 
 class BlogListView(ListView):
@@ -8,6 +12,25 @@ class BlogListView(ListView):
     template_name = 'home.html'
     context_object_name = 'list_of_posts'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
